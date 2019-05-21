b47fc1fe780a390c17bc94ea3057cf65babbee6f
Lucien Du <david.xelnaga@gmail.com>
2019-05-20 22:48:29 -0700

    Proof of concept

---
+++ FStrStat/Demo/Program.cs
@@ -0,0 +1,100 @@
+﻿using FStrStat.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MathNet.Numerics.Random;
+using MathNet.Numerics.Distributions;
+
+namespace Demo
+{
+    public abstract class FailsafeProcess
+    {
+        private long failureCount;
+        private double successRate;
+
+        public bool Test(double sample)
+        {
+            successRate = AdjuestSuccessRate(failureCount);
+            if (sample < successRate)
+            {
+                failureCount = 0;
+                return true;
+            }
+            else
+            {
+                failureCount++;
+                return false;
+            }
+        }
+
+        protected abstract double AdjuestSuccessRate(long failureCount);
+    }
+
+    class Example:
+        FailsafeProcess
+    {
+        private const double DefaultRate = 0.02;
+        private const double Step = 0.02;
+        private const long FailureThreshold = 50;
+
+        protected override double AdjuestSuccessRate(long failureCount)
+        {
+            if (failureCount < FailureThreshold)
+            {
+                return DefaultRate;
+            }
+            else
+            {
+                var adjustment = Step * (failureCount - FailureThreshold);
+                return Math.Min(DefaultRate + adjustment, 1.0);
+            }
+            //return DefaultRate;
+        }
+    }
+    class Program
+    {
+        const int ThreadCount = 6;
+        const long TestCount = 50000000;
+        static async Task Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var param = new SimulatorParams()
+            {
+                RandomSourceFactory = () => new ContinuousUniform(0, 1, new MersenneTwister()),
+                RenewRandomSourceAfter = 2L << 30,
+                TestFactory = () => new Example().Test,
+            };
+            var sim = new Simulator(param);
+            var source = new CancellationTokenSource();
+            for(int i = 0; i < ThreadCount; ++i)
+            {
+                tasks.Add(sim.RunSimulation(source.Token));
+            }
+            for(; ; )
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                double mean, min, max, stddev;
+                long count;
+                lock(sim.Accumulator)
+                {
+                    mean = sim.Accumulator.Mean;
+                    min = sim.Accumulator.Minimum;
+                    max = sim.Accumulator.Maximum;
+                    stddev = sim.Accumulator.StandardDeviation;
+                    count = sim.Accumulator.Count;
+                }
+
+                Console.WriteLine(
+                    $"Count:{count}, Mean: {mean}±{stddev}, Min:{min}, Max:{max}");
+
+                if(count >= TestCount)
+                {
+                    source.Cancel();
+                    break;
+                }
+            }
+            await Task.WhenAny(Task.WhenAll(tasks), Task.Delay(Timeout.InfiniteTimeSpan, source.Token));
+        }
+    }
+}
---
+++ FStrStat/FStrStat.Core/Simulator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
+
+namespace FStrStat.Core
+{
+    public class SimulatorParams
+    {
+        public Func<IContinuousDistribution> RandomSourceFactory { get; set; }
+
+        public Func<Func<double, bool>> TestFactory { get; set; }
+
+        public long RenewRandomSourceAfter { get; set; }
+
+    }
+    public class Simulator
+    {
+        private Func<IContinuousDistribution> randomSourceFactory;
+        private Func<Func<double, bool>> testFactory;
+        private long renewRandomSourceAfter;
+
+        public RunningStatistics Accumulator { get; } = new RunningStatistics();
+
+        public Simulator(SimulatorParams param)
+        {
+            randomSourceFactory = param.RandomSourceFactory ?? throw new ArgumentNullException();
+            testFactory = param.TestFactory ?? throw new ArgumentNullException();
+            renewRandomSourceAfter = param.RenewRandomSourceAfter > 0 ? param.RenewRandomSourceAfter : throw new ArgumentException();
+        }
+
+        public Task RunSimulation(CancellationToken token)
+        {
+            var test = testFactory();
+            var randSource = randomSourceFactory();
+            var sampleCount = 0;
+            void CheckRandSourceRenew()
+            {
+                if(sampleCount > renewRandomSourceAfter)
+                {
+                    randSource = randomSourceFactory();
+                }
+            }
+            double GetNextFailureStringLength()
+            {
+                double failureStringLength = 0;
+                for(; ; )
+                {
+                    token.ThrowIfCancellationRequested();
+                    if(test(randSource.Sample()))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ++failureStringLength;
+                        ++sampleCount;
+                        CheckRandSourceRenew();
+                    }
+                    
+                }
+                return failureStringLength;
+            }
+
+            void MainLoop()
+            {
+                for (; ; )
+                {
+                    token.ThrowIfCancellationRequested();
+                    ReportFailureStringLength(GetNextFailureStringLength());
+                }
+            }
+
+            return Task.Run(MainLoop, token);
+        }
+
+        private void ReportFailureStringLength(double length)
+        {
+            lock(Accumulator)
+            {
+                Accumulator.Push(length);
+            }
+        }
+    }
+}
